640b4a9c5ae40ee4754ed2691730bbed1660b324
Glenn Jönsson <glennj83@hotmail.com>
2020-09-17 15:36:44 +0200

    Init MyBookingsView

---
+++ Client/HolidayMaker.Client/HolidayMaker.Client/ViewModel/MyBookingsViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HolidayMaker.Client.Model;
+using HolidayMaker.Client.View;
+
+namespace HolidayMaker.Client.ViewModel
+{    
+    public class MyBookingsViewModel
+    {
+        //Booking myBooking;
+        //BookedRoom bookedRoom;
+        public MyBookingsViewModel()
+        {
+            //myBooking = new Booking();
+
+            //myBooking.BookedRooms.Add(new BookedRoom());
+
+        }
+        
+
+    }
+}
