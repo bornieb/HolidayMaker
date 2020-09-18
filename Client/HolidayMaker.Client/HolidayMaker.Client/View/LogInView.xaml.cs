6a9a8d7e43426996c1a5119015f286ce103c34db
Jennie Fogwall <j_stenvall@hotmail.se>
2020-09-18 11:34:13 +0200

    autentisera användare

---
+++ Client/HolidayMaker.Client/HolidayMaker.Client/View/LogInView.xaml.cs
@@ -0,0 +1,41 @@
+﻿using HolidayMaker.Client.Service;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace HolidayMaker.Client.View
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class LogInView : Page
+    {
+        UserService userService = new UserService();
+
+        public LogInView()
+        {
+            this.InitializeComponent();
+        }
+
+        private async void LogInButton_Click(object sender, RoutedEventArgs e)
+        {
+            string userName = UserNameTextBox.Text;
+            string password = PasswordTextBox.Password;
+
+            await userService.LogIn(userName, password);
+        }
+    }
+}
